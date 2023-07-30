9e7a5493e1b0367046085d3bde31512921130428
Moisés Vicente Bezerra de Souza <93298283+MoisesSouza16Tech@users.noreply.github.com>
2023-07-30 06:14:39 -0300

    Create loader.js

---
+++ assets/js/loader.js
@@ -0,0 +1,8 @@
+// Seu JavaScript
+window.addEventListener("load", function () {
+    // Após 4 segundos, adiciona a classe "hidden" ao elemento com o id "loader"
+    setTimeout(function () {
+      document.getElementById("loader").classList.add("hidden");
+    }, 4000);
+  });
+  
